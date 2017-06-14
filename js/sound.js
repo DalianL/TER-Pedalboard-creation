02c7c9d391d00e7854c7d0ba488a3f6bd4ae09cd
Matthieu Barzellino <matthieu.barzellino@etu.unice.fr>
2017-06-14 17:15:41 +0200

    Ajout de l'enregistrement micro

--- js/sound.js
+++ js/sound.js
@@ -1,6 +1,10 @@
 var pzContext;
 var mediaSource;
 var audioDestination;
+var mediaSourceM;
+var mediaSRC;
+
+
 
 function soundHandler() {
   let soundSample = document.querySelector('#soundSample');
@@ -14,13 +18,28 @@ function soundHandler() {
   // Method 2 to connect source to destination
   //pedal.connect(pzContext.createMediaElementSource(soundSample),pzContext.destination);
 
+
+if (navigator.mediaDevices.getUserMedia) {
+  console.log('getUserMedia supported.');
+  navigator.mediaDevices.getUserMedia({ 
+  audio: true
+  }).then(function(stream) {
+      mediaSourceM = pzContext.createMediaStreamSource(stream);
+  }).catch(function(err) {
+  // handle the error
+  console.log(err.name + ": " + err.message);
+  });
+}
+
 };
 
 function soundNodeConnection(p1,p2) {
   if (p1.id == "pedalIn" && p2.id == "pedalOut") {
     mediaSource.connect(audioDestination);
+    mediaSourceM.connect(audioDestination);
   } else if (p1.id == "pedalIn") {
     mediaSource.connect(p2.elem.soundNodeIn);
+    mediaSourceM.connect(p2.elem.soundNodeIn);
   } else if (p2.id == "pedalOut") {
     p1.elem.soundNodeOut.connect(audioDestination);
   } else {
@@ -31,11 +50,14 @@ function soundNodeConnection(p1,p2) {
 function soundNodeDisconnection(p1,p2) {
   if (p1.id == "pedalIn" && p2.id == "pedalOut") {
     mediaSource.disconnect(audioDestination);
+    mediaSourceM.disconnect(audioDestination);
   } else if (p1.id == "pedalIn") {
     mediaSource.disconnect(p2.elem.soundNodeIn);
+    mediaSourceM.disconnect(p2.elem.soundNodeIn);
   } else if (p2.id == "pedalOut") {
     p1.elem.soundNodeOut.disconnect(audioDestination);
   } else {
     p1.elem.soundNodeOut.disconnect(p2.elem.soundNodeIn);
   }
 }
+
